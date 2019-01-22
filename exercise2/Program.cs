ed1e2598161c1920893982973e54effc5a1ef44a
reddyin12 <gindra.kp86@gmail.com>
2019-01-22 11:32:37 -0800

    exercise 2

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -6,7 +6,30 @@ namespace exercise2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exercise 2
+            // Input a number (n) from the user and check if it is even or odd.
+
+            // Write your code here
+
+
+            Console.WriteLine("enter a number to check even or odd");
+            int a = int.Parse(Console.ReadLine());
+            if(a%2==0)
+            {
+                Console.WriteLine("Value is even");
+
+            }
+
+            else if(a%2!=0)
+            {
+                Console.WriteLine("value is odd");
+            }
+            ConsoleKeyInfo  info = Console.ReadKey();
+            Console.WriteLine(info);
+
+
+            
+
         }
     }
 }
